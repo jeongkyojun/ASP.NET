9b66f80f019ae5f54b49e6b4b1d12f04e4ce8ce4
jeongkyojun <kyojun2002@naver.com>
2020-11-23 21:30:26 +0900

    WebEx03 생성

---
+++ ASP.Net 4.0/WebEx3/BMI.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class BMI : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Calc_ServerClick(object sender, EventArgs e)
+    {
+        decimal height = Decimal.Parse(Height.Value) / 100; // cm를 m로 환산
+        decimal weight = Decimal.Parse(Weight.Value);
+
+        decimal BMI = weight / (height*height);
+        Result.InnerText = "체질량지수(BMI) : " + BMI.ToString().Substring(0,6);
+    }
+}
