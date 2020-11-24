0cc286926c5699e132d6a7225ffbd09ca382dcea
jeongkyojun <kyojun2002@naver.com>
2020-11-24 19:18:09 +0900

    select속성 추가

---
+++ ASP.Net 4.0/WebEx3/BMI2.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="BMI2.aspx.cs" Inherits="BMI2" %>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>
+            체질량 지수 산출기
+        </title>
+    </head>
+    <body>
+        <form id="WebForm" runat="server">
+            <div align="center">
+                신장 : &nbsp;
+                <input id="Height" type="text" runat ="server"/>
+                &nbsp;
+                <select id="Measure" runat="server"/>
+                &nbsp;&nbsp;몸무게 :&nbsp;
+                <input id="Weight" type="text" runat="server"/>
+                &nbsp;kg
+                <br /><br />
+                <input id="Calc" type="submit" value="산출하기" 
+                    runat ="server" onserverclick="Calc_ServerClick"/>
+                <br />
+                <br />
+                <p id ="Result" runat="server"> "체질량지수(BMI) : " </p>
+                <!--<p> 블록 : 형식이 있는 텍스트블록을 웹페이지에 넣을때 사용한다.-->
+            </div>
+        </form>
+    </body>
+</html>
---
+++ ASP.Net 4.0/WebEx3/BMI2.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class BMI2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            //IsPostBack은 처음 열리는 페이지인지 확인하는 속성(false인 경우 처음 열리는 페이지이다.)
+            Measure.Items.Add(new ListItem("센티미터(cm)","0.01"));
+            Measure.Items.Add(new ListItem("피트(feet)","0.3048"));
+            Measure.Items.Add(new ListItem("미터(m)","1"));
+        }
+    }
+    protected void Calc_ServerClick(object sender, EventArgs e)
+    {
+        try
+        {
+            ListItem item = Measure.Items[Measure.SelectedIndex];
+            decimal height = Decimal.Parse(Height.Value) * Decimal.Parse(item.Value);
+            decimal weight = Decimal.Parse(Weight.Value);
+
+            decimal BMI = weight / (height * height);
+            Result.InnerText = "체질량지수(BMI) : " + BMI.ToString().Substring(0, 6);
+        }
+        catch(Exception exp)
+        {
+
+        }
+    }
+}
